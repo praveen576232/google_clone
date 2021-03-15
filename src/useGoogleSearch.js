f93f48b3aecdaaa76d6117027f4472675d7bf0e8
praveen576232 <48440981+praveen576232@users.noreply.github.com>
2021-03-15 14:03:11 +0530

    bug fix

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,12 +1,13 @@
 import React,{useState, useEffect} from 'react'
-import API_KEY from './api_key';
-const CONTEXT_KEY ="98254c7d03bae35c0";
+
+
 const useGoogleSearch=(term)=> {
     const [data,setdata]=useState(null)
     useEffect(() => {
+       
        const featchData = async() => {
            fetch(
-               `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+               `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=${process.env.REACT_APP_CONTEXT_KEY}&q=${term}`
            ).then(res => res.json())
            .then(result =>{
             setdata(result)
